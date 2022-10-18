d1ff1e7e84d00014ff87bb4b1cf520a15af1bef6
AndrewKeYanzhe <keyanzhe@gmail.com>
2022-10-18 10:39:39 +0100

    feat: program to leave starting box

---
+++ line_follower/leave_start/leave_start.ino
@@ -0,0 +1,195 @@
+// leave initial box and turn right onto white line
+
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Select which 'port' - currently left = M1 and right = M2
+Adafruit_DCMotor *left_motor = AFMS.getMotor(1);
+Adafruit_DCMotor *right_motor = AFMS.getMotor(2);
+
+// Declaring variables
+
+int default_speed = 192; //75% of max speed
+
+bool sensor_1;
+bool sensor_2;
+bool sensor_3;
+bool sensor_4;
+
+bool far_left_white = false;
+bool left_white = false;
+bool right_white = false;
+bool far_right_white = false;
+
+bool on_line = false;
+
+int white_line_crossed = 0;
+
+int left_motorSpeed = 0;
+int right_motorSpeed = 0;
+
+void set_motor_speed(char motor_label,int new_speed) {
+  //read old speed
+  int old_speed = NULL;
+  switch(motor_label){
+    case 'L':
+      old_speed = left_motorSpeed;
+      break;
+    case 'R':
+      old_speed = right_motorSpeed;
+      break;
+  }
+
+  //set new speed
+  if (new_speed==old_speed){
+    return;
+  }
+  else {
+    // send a command that sets the  motor speed to the new speed, then ...
+    switch(motor_label){
+    case 'L':
+      left_motor->setSpeed(new_speed);
+      left_motorSpeed = new_speed;
+      break;
+    case 'R':
+      right_motor->setSpeed(new_speed);
+      right_motorSpeed = new_speed;
+      break;
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  AFMS.begin();
+
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1);
+  }
+  Serial.println("Motor Shield found.");
+
+  set_motor_speed('L',default_speed);
+  left_motor->run(FORWARD);
+  // turn on motor
+  left_motor->run(RELEASE);  
+
+  set_motor_speed('R',default_speed);
+  right_motor->run(FORWARD);
+  // turn on motor
+  right_motor->run(RELEASE); 
+
+  Serial.println("going forward");
+
+
+
+}
+
+void read_sensors(){
+  sensor_1 = digitalRead(0);
+  sensor_2 = digitalRead(1);
+  sensor_3 = digitalRead(2);
+  sensor_4 = digitalRead(3);
+
+  if (sensor_2 == HIGH) {
+    Serial.println("sensor 1 BLACK");
+  }
+  else{
+    Serial.println("sensor 1 WHITE");
+  }
+  
+  
+  far_left_white = sensor_1 == LOW;
+  left_white = sensor_2 == LOW;
+  right_white = sensor_3 == LOW;
+  far_right_white = sensor_4 == LOW;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  read_sensors();
+  
+
+  if (left_white && right_white && !far_left_white && !far_right_white){
+    Serial.println("found line");
+    on_line = true;
+    follow_line();
+  }
+
+
+  delay(1);
+
+}
+
+void follow_line(){
+    // put your main code here, to run repeatedly:
+
+  read_sensors();
+
+  if (left_white && right_white && !far_left_white && !far_right_white){
+    Serial.println("continue forward");
+    set_motor_speed('L',default_speed);
+    set_motor_speed('R',default_speed);
+  }
+
+  else if (left_white && right_white && far_left_white && far_right_white){
+    Serial.println("crossed white line");
+    white_line_crossed = white_line_crossed + 1;
+    check_state();
+  }
+
+  else if (left_white && !right_white){
+    Serial.println("nudge left");
+    set_motor_speed('L',default_speed*0.9);
+    set_motor_speed('R',default_speed);
+  }
+  else if (!left_white && right_white){
+    Serial.println("nudge right");
+    set_motor_speed('L',default_speed);
+    set_motor_speed('R',default_speed*0.9);
+  }
+  
+
+  delay(1);
+  follow_line();
+}
+
+void stop_motors(){
+    set_motor_speed('L',0);
+    set_motor_speed('R',0);
+}
+
+void check_state(){
+  Serial.println("checking state");
+  if (white_line_crossed == 1){
+    stop_motors();
+    turn_right();
+
+  }
+
+}
+
+void turn_right(){
+  read_sensors();
+  Serial.println("turn right");
+
+  stop_motors();
+
+  set_motor_speed('L',default_speed);
+
+  if (left_white && right_white && !far_left_white && !far_right_white){
+    Serial.println("found line");
+    stop_motors();
+
+    on_line = true;
+    follow_line();
+  }
+
+}
