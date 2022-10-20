9458135f2fbb54849ad010c884082ed69619f90f
AndrewKeYanzhe <keyanzhe@gmail.com>
2022-10-20 14:59:05 +0100

    2_sensors: calibrate speed so it turns corners

--- line_follower/line_follower_2_sensors/line_follower_2_sensors.ino
+++ line_follower/line_follower_2_sensors/line_follower_2_sensors.ino
@@ -5,11 +5,11 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' - currently left = M1 and right = M2
 Adafruit_DCMotor *left_motor = AFMS.getMotor(1);
-Adafruit_DCMotor *right_motor = AFMS.getMotor(2);
+Adafruit_DCMotor *right_motor = AFMS.getMotor(4);
 
 // Declaring variables
 
-int default_speed = 192; //75% of max speed
+int default_speed = 150; //current calibration is without battery weight (battery held in hand). If robot weight changes, speed must be reduced so it wont overshoot line
 
 bool SensorOne;
 bool SensorTwo;
@@ -63,14 +63,14 @@ void setup() {
   Serial.println("Motor Shield found.");
 
   set_motor_speed('L',default_speed);
-  left_motor->run(FORWARD);
+  left_motor->run(BACKWARD);
   // turn on motor
-  left_motor->run(RELEASE);  
+  // left_motor->run(RELEASE);  
 
   set_motor_speed('R',default_speed);
-  right_motor->run(FORWARD);
+  right_motor->run(BACKWARD);
   // turn on motor
-  right_motor->run(RELEASE);  
+  // right_motor->run(RELEASE);  
 
 
 
@@ -100,13 +100,13 @@ void loop() {
 
   else if (left_black && !right_black){
     Serial.println("turn right");
-    set_motor_speed('L',default_speed);
-    set_motor_speed('R',default_speed*0.9);
+    set_motor_speed('L',255);
+    set_motor_speed('R',0.3*default_speed);
   }
   else if (!left_black && right_black){
     Serial.println("turn left");
-    set_motor_speed('L',default_speed*0.9);
-    set_motor_speed('R',default_speed);
+    set_motor_speed('L',0.3*default_speed);
+    set_motor_speed('R',255);
   }
 
   delay(1);
