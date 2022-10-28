f507a1a29898486af6dd295302b1f147441516a4
AndrewKeYanzhe <keyanzhe@gmail.com>
2022-10-28 15:56:13 +0100

    refactor (Hall Sensor Test)

--- Hall Sensor Test/Hall Sensor Test.ino
+++ Hall Sensor Test/Hall Sensor Test.ino
@@ -1,27 +1,13 @@
-/*
-* AD22151G is a linear magnetic field transducer Analog Sensor Print on 15 LED's Bar-Graph
-*visit https//www.twovolt.com for code, schematic, PCB layout
-
-*/
-
-// these constants won’t change:
-const int analogPin = 13; // the pin that the potentiometer is attached to
-// }; // an array of pin numbers to which LEDs are attached
+const int hallEffectPin = 13;
 
 void setup() {
   Serial.begin(9600);  
-  // AFMS.begin();
 
-  // if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-  //   Serial.println("Could not find Motor Shield. Check wiring.");
-  //   while (1);
-  // }
-  // Serial.println("Motor Shield found.");
 }
 
 void loop() {
   // read the potentiometer:
-  int sensorReading = !digitalRead(analogPin);
+  int sensorReading = !digitalRead(hallEffectPin);
   // print the sensor reading
   Serial.println(sensorReading);
   delay(100);
